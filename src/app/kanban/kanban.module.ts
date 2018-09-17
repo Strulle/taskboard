92724a64a5336f60de5dcdf33a9f19635f96a57e
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-09-17 10:34:58 +0200

    prettier: use single quotes

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
-import { BoardComponent } from "./board.component";
-import { DescribeCardComponent } from "./describe-card/describe-card.component";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BoardComponent } from './board.component';
+import { DescribeCardComponent } from './describe-card/describe-card.component';
 
 @NgModule({
   imports: [CommonModule],
