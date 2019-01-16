12bbea5f5f0af04eefc3080b7147aee2c43e917c
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-16 12:24:04 +0100

    task-board: add multiple test cases for invalid inputs

--- src/app/task-board/task-quick-add/task-quick-add.component.spec.ts
+++ src/app/task-board/task-quick-add/task-quick-add.component.spec.ts
@@ -2,12 +2,25 @@ import { TaskQuickAddComponent } from './task-quick-add.component';
 
 describe('<tb-task-quick-add>', () => {
   describe('When no title is given', () => {
-    it('should not emit a draft for the task', () => {
+    it.each([null, undefined, '', ' '])(
+      'should not emit a draft for the task ("%s")',
+      notAValue => {
+        const sut = new TaskQuickAddComponent();
+        const output = jest.spyOn(sut.create, 'emit');
+
+        sut.title.setValue(notAValue);
+        expect(output).not.toBeCalled();
+      }
+    );
+  });
+
+  describe('When title is given', () => {
+    it('should emit a draft for the task', () => {
       const sut = new TaskQuickAddComponent();
       const output = jest.spyOn(sut.create, 'emit');
 
-      sut.title.setValue('');
-      expect(output).not.toBeCalled();
+      sut.title.setValue('a');
+      expect(output).toBeCalled();
     });
   });
 });
--- src/app/task-board/task-quick-add/task-quick-add.component.ts
+++ src/app/task-board/task-quick-add/task-quick-add.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'tb-task-quick-add',
   templateUrl: './task-quick-add.component.html',
   styleUrls: ['./task-quick-add.component.scss']
 })
-export class TaskQuickAddComponent implements OnInit {
+export class TaskQuickAddComponent {
   title = new FormControl('');
 
   @Output() create = new EventEmitter();
 
-  constructor() {}
-
-  ngOnInit() {}
+  constructor() {
+    this.title.valueChanges
+      .pipe(filter(value => !!value && value.trim().length > 0))
+      .subscribe(value => this.create.emit(value));
+  }
 }
