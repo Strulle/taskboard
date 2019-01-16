b94d119cf109c22c524dbbc896863bc01a5dee68
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-16 10:48:02 +0100

    task-board: add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { TaskBoardModule } from './task-board/task-board.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, TaskBoardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
