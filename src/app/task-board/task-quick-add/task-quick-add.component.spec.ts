e39bf5180ea4acce7d68157e543ce55fd2ab91f9
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-16 13:47:00 +0100

    test(task-quick-add): ensure that subscription gets terminated

--- src/app/task-board/task-quick-add/task-quick-add.component.spec.ts
+++ src/app/task-board/task-quick-add/task-quick-add.component.spec.ts
@@ -1,13 +1,19 @@
+import { observers } from '@test/index';
 import { TaskQuickAddComponent } from './task-quick-add.component';
 
 describe('<tb-task-quick-add>', () => {
+  let sut: TaskQuickAddComponent;
+  let output: jest.SpyInstance<(value?: {}) => void>;
+
+  beforeEach(() => {
+    sut = new TaskQuickAddComponent();
+    output = jest.spyOn(sut.create, 'emit');
+  });
+
   describe('When no title is given', () => {
     it.each([null, undefined, '', ' '])(
       'should not emit a draft for the task ("%s")',
       notAValue => {
-        const sut = new TaskQuickAddComponent();
-        const output = jest.spyOn(sut.create, 'emit');
-
         sut.title.setValue(notAValue);
         expect(output).not.toBeCalled();
       }
@@ -16,11 +22,13 @@ describe('<tb-task-quick-add>', () => {
 
   describe('When title is given', () => {
     it('should emit a draft for the task', () => {
-      const sut = new TaskQuickAddComponent();
-      const output = jest.spyOn(sut.create, 'emit');
-
       sut.title.setValue('a');
       expect(output).toBeCalled();
     });
+  });
+
+  afterEach(() => {
+    sut.ngOnDestroy();
+    observers([sut.title.valueChanges]).verify();
   });
 });
--- src/app/task-board/task-quick-add/task-quick-add.component.ts
+++ src/app/task-board/task-quick-add/task-quick-add.component.ts
@@ -1,20 +1,31 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
-import { filter } from 'rxjs/operators';
+import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tb-task-quick-add',
   templateUrl: './task-quick-add.component.html',
   styleUrls: ['./task-quick-add.component.scss']
 })
-export class TaskQuickAddComponent {
+export class TaskQuickAddComponent implements OnDestroy {
+  destroy$$ = new Subject();
+
   title = new FormControl('');
 
   @Output() create = new EventEmitter();
 
   constructor() {
     this.title.valueChanges
-      .pipe(filter(value => !!value && value.trim().length > 0))
+      .pipe(
+        takeUntil(this.destroy$$),
+        filter(value => !!value && value.trim().length > 0)
+      )
       .subscribe(value => this.create.emit(value));
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
   }
 }
