9a470de59b14bdba23abb47c3f3cb95aa48478e1
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-01-18 13:28:30 +0100

    quick-add: remove mechanism unsubscribing subscriptions

--- src/app/task-board/task-quick-add/task-quick-add.component.ts
+++ src/app/task-board/task-quick-add/task-quick-add.component.ts
@@ -1,23 +1,15 @@
-import { Component, EventEmitter, OnDestroy, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'tb-task-quick-add',
   templateUrl: './task-quick-add.component.html',
   styleUrls: ['./task-quick-add.component.scss']
 })
-export class TaskQuickAddComponent implements OnDestroy {
-  private _destroy$$ = new Subject();
-
+export class TaskQuickAddComponent {
   title = new FormControl('', [Validators.required]);
 
   @Output() create = new EventEmitter();
-
-  ngOnDestroy(): void {
-    this._destroy$$.next();
-    this._destroy$$.complete();
-  }
 
   emitDraft() {
     this.create.emit(this.title.value);
