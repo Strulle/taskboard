f465005c0aaeaac26fc9df835232dfb2837e553e
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-02-08 18:12:28 +0100

    feat(search): exports <search-box>

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { SearchBoxComponent } from './search-box/search-box.component';
 
 @NgModule({
   declarations: [SearchBoxComponent],
-  imports: [CommonModule]
+  imports: [CommonModule],
+  exports: [SearchBoxComponent]
 })
 export class SearchModule {}
